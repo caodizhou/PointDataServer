7fd456cc2dc70232d2674ee89f83872c107f65fa
cdz <dizhoucao@gmail.com>
2017-11-13 15:18:54 +0800

    增加缩放时2d框的变动

--- web/Resource/JS/Pointcloud.js
+++ web/Resource/JS/Pointcloud.js
@@ -207,7 +207,7 @@ pointCloud3d.prototype = {
                 }
                 particles.geometry = geometry;
                 // time = now;
-                if (!$("#onoffswitch2").is(':checked')){
+                if (!$("#onoffswitch2").is(':checked')) {
                     pointCloud.updateCube();
                 }
                 dataindex = scale.dataindex;
@@ -247,13 +247,13 @@ pointCloud3d.prototype = {
         pointCloud.camera.position.z = z;
         // pointCloud.camera.position.z = 800;
     },
-    creatCube: function (id, x, y, w, l, r ,z ,h) {
-        var cubeh = h||0;
+    creatCube: function (id, x, y, w, l, r, z, h) {
+        var cubeh = h || 0;
         var geometry = new THREE.BoxGeometry(w, l, cubeh);
         var cube = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 'green', wireframe: true}));
         cube.position.x = x;
         cube.position.y = y;
-        cube.position.z = z||0;
+        cube.position.z = z || 0;
         cube.rotation.z = -r * Math.PI / 180;
         this.scene.add(cube);
         return cube;
@@ -298,7 +298,7 @@ pointCloud3d.prototype = {
         boxmap.forEach(function (value, key, map) {
             var box = value.get(pointCloud.dataindex);
             if (box != null) {
-                pointCloud.creatCube(key, box.x, box.y, box.w, box.l, box.r,box.z,box.h);
+                pointCloud.creatCube(key, box.x, box.y, box.w, box.l, box.r, box.z, box.h);
             }
         })
     },
@@ -418,7 +418,7 @@ pointCloud3d.prototype = {
             var height = Number(rect.attr("height"));
             var id = 1;
             var boxmap = pointCloud.boxControl.boxmap;
-            while (boxmap.get(id) != null) {
+            while (boxmap.get(id) != null && boxmap.get(id).size != 0) {
                 id++;
             }
             var box = new Box(x + width / 2, y + height / 2, width, height, rotate, id, pointCloud.dataindex);
@@ -436,11 +436,11 @@ pointCloud3d.prototype = {
                     $("#BoxIdInput").val(id);
                     d3.select("#BoxIdInput").property("oldId", id);
                     $("#xInput").val(box.x);
-                    $("#yInput").val(Math.abs(box.y));
-                    $("#lengthInput").val(box.l);
+                    $("#yInput").val(box.y);
+                    $("#lengthInput").val(Math.abs(box.l));
                     $("#widthInput").val(box.w);
                 });
-            document.removeEventListener("dblclick",HandleBrushDblClick,true);
+            document.removeEventListener("dblclick", HandleBrushDblClick, true);
             // pointCloud.creatCube(0,x1,y1,x2,y2,rotate);
 
         }
@@ -515,42 +515,48 @@ pointCloud3d.prototype = {
                     d3.selectAll("g.brush").remove();
                 }
             })
-        var start = {x:0,y:0};
+        var start = {x: 0, y: 0};
         var svgdom = svg._groups[0][0];
-        svgdom.addEventListener("mousedown",HandleLeftMouseDown);
+        svgdom.addEventListener("mousedown", HandleLeftMouseDown);
         function HandleLeftMouseDown(event) {
             start.x = event.clientX;
             start.y = event.clientY;
-            svgdom.addEventListener("mousemove",HandleLeftMouseMove);
-            svgdom.addEventListener("mouseup",HandleLeftMouseUp);
+            svgdom.addEventListener("mousemove", HandleLeftMouseMove);
+            svgdom.addEventListener("mouseup", HandleLeftMouseUp);
         }
+
         function HandleLeftMouseMove(event) {
             var delta = {
-                x:event.clientX -start.x,
-                y:event.clientY -start.y
+                x: event.clientX - start.x,
+                y: event.clientY - start.y
             };
             start.x = event.clientX;
             start.y = event.clientY;
 
-            svg.selectAll("rect.brushRect").attr("originX",function () {
-              return  Number(d3.select(this).attr("originX"))+delta.x;
-            }).attr("originY",function () {
-              return  Number(d3.select(this).attr("originY"))+delta.y;
+            svg.selectAll("rect.brushRect").attr("originX", function () {
+                return Number(d3.select(this).attr("originX")) + delta.x;
+            }).attr("originY", function () {
+                return Number(d3.select(this).attr("originY")) + delta.y;
             });
-            svg.selectAll("rect").attr("x",function () {
-              return  Number(d3.select(this).attr("x"))+delta.x;
-            }).attr("y",function () {
-              return  Number(d3.select(this).attr("y"))+delta.y;
-            }).attr("transform-origin",function () {
+            svg.selectAll("rect").attr("x", function () {
+                return Number(d3.select(this).attr("x")) + delta.x;
+            }).attr("y", function () {
+                return Number(d3.select(this).attr("y")) + delta.y;
+            }).attr("transform-origin", function () {
                 var id = d3.select(this).attr("mainId");
-                var brushrect =id&&d3.select("rect#"+id)||d3.select(this);
-              return brushrect.attr("originX")+" "+brushrect.attr("originY");
+                var brushrect = id && d3.select("rect#" + id) || d3.select(this);
+                return brushrect.attr("originX") + " " + brushrect.attr("originY");
             });
 
         }
-        function HandleLeftMouseUp(event){
-            svgdom.removeEventListener("mousemove",HandleLeftMouseMove);
-            svgdom.removeEventListener("mouseup",HandleLeftMouseUp);
+
+        function HandleLeftMouseUp(event) {
+            svgdom.removeEventListener("mousemove", HandleLeftMouseMove);
+            svgdom.removeEventListener("mouseup", HandleLeftMouseUp);
+        }
+        svgdom.addEventListener("wheel",HandleMouseWheel,false);
+        function HandleMouseWheel(event) {
+            pointCloud.updateBrushOnWheel();
         }
 
     },
@@ -558,12 +564,12 @@ pointCloud3d.prototype = {
         var pointCloud = this;
         var svg = d3.select(this.container).select("svg");
         var boxmap = pointCloud.boxControl.boxmap;
-        boxmap.forEach(function (value, key, map) {
+        boxmap.forEach(function (value, key) {
             var box = value.get(pointCloud.dataindex);
             if (box != null) {
                 var p1 = pointCloud.trans3dto2d(box.x - box.w / 2, box.y - box.l / 2, 0);
                 var p2 = pointCloud.trans3dto2d(box.x + box.w / 2, box.y + box.l / 2, 0);
-                MyRect(p1.x, p1.y,Math.abs( p2.x - p1.x), Math.abs(p2.y - p1.y), "rect"+key, box.r, svg);
+                MyRect(p1.x, p1.y, Math.abs(p2.x - p1.x), Math.abs(p2.y - p1.y), "rect" + key, box.r, svg);
             }
         })
         d3.selectAll(".brushRect")
@@ -579,10 +585,13 @@ pointCloud3d.prototype = {
                 $("#widthInput").val(box.w);
             })
     },
+    updateBrushOnWheel: function () {
+        this.refrushbrush();
+    },
     removebrush: function () {
         d3.select(this.container).selectAll("svg").remove();
     },
-    refrushbrush:function () {
+    refrushbrush: function () {
         var svg = d3.select(this.container).select("svg.pointCloud3d");
         svg.selectAll("rect").remove();
         svg.selectAll("g").remove();
@@ -605,7 +614,7 @@ pointCloud3d.prototype = {
 
         };
     }
-    
+
 }
 BoxControl = function (boxmap) {
     this.boxmap = boxmap || new Map;
@@ -660,12 +669,12 @@ Box.prototype = {
         var zmin = Number.MAX_VALUE;
         var zmax = -Number.MAX_VALUE;
         data.forEach(function (v) {
-            if (box.isInbox(v.x*10,v.y*10)) {
-                if (v.z*10 > zmax) {
-                    zmax = v.z*10;
+            if (box.isInbox(v.x * 10, v.y * 10)) {
+                if (v.z * 10 > zmax) {
+                    zmax = v.z * 10;
                 }
-                if (v.z*10 < zmin) {
-                    zmin = v.z*10;
+                if (v.z * 10 < zmin) {
+                    zmin = v.z * 10;
                 }
             }
 
